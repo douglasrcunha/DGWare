421f27de861988903059c9195fd7735460d09230
Douglas Cunha <douglasr.cunha@hotmail.com>
2024-01-17 16:16:11 -0300

    closes #5 - Configure settings.py for the database (postgresql)

--- DGWare/settings.py
+++ DGWare/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'DGWare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dgware_db',
+	'USER': 'postgres',
+	'PASSWORD': '*682970*',
+	'HOST': 'localhost',
     }
 }
 
